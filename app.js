b921dc488a03003b1a247690caa4b7bb875d9023
HimanshuMamgain <himanshum@vendify.in>
2024-02-20 23:24:35 +0530

    ft: Get and Post request created

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+
+const app = express();
+const port = 7777;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+// Custom middleware
+// app.use((req,res,next)=>{
+//     console.log("Custom middleware 1");
+//     next();
+// })
+// app.use((req,res,next)=>{
+//     console.log("Custom middleware 2");
+//     next();
+// })
+
+let students = [
+    {
+        'id': 1,
+        'name': "Himanshu",
+        "age": 21,
+        "salary": 25000,
+    },
+    {
+        'id': 2,
+        'name': "Yogesh",
+        "age": 23,
+        "salary": 15000,
+    },
+    {
+        'id': 3,
+        'name': "Zaki",
+        "age": 25,
+        "salary": 10000,
+    }
+]
+
+app.route('/')
+.get((req,res)=>{
+    return res.json({'message': 'Node js started'});
+})
+
+// HTTP methods
+// 1) get
+app.get('/students', (req,res)=>{
+    return res.json({"data": students});
+})
+
+app.get('/student/:id', (req, res) => {
+    let id = req.params.id
+    let student = students.filter((value) => {
+        return value.id == id
+    })
+    return res.json({"data": student});
+})
+// 2) post
+app.post('/addStudent', (req,res)=>{
+    let id = req.body.id;
+    let student = students.filter((value) => {
+        return value.id == id
+    })
+    if(student != null){
+        students.push({...req.body});
+        return res.json({'data': students})
+    }
+    return res.json({'message': 'Student already existed'});
+})
+// app.post('/addStudent', (req,res)=>{
+//     let body = req.body;
+//     console.log(body)
+//     return res.json({'message': 'Working with post request'});
+// })
+// 3) put
+// 4) patch
+// 5) delete
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+})
